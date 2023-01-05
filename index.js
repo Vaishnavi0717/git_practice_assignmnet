d5594e69b4fd34853473945d20d27c600d6efa3d
Vaishnavi0717 <vaishnavipunekar0@gmail.com>
2023-01-05 13:23:08 +0530

    Problem_on_Prime_number_and_Palindrome

---
+++ index.js
@@ -0,0 +1,14 @@
+let str="masai";
+let rev="";
+
+for(let i=str.length; i>=0; i--){
+rev=rev+str[i];
+}
+if(rev==str){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
+
+
---
+++ index1.js
@@ -0,0 +1,13 @@
+let num=5; 
+let count=0; 
+
+for(let i=1; i<=num; i++){
+if(num%i==0){
+count++;
+}
+} if(count==0){
+console.log("yes");
+}
+else{
+console.log("No");
+}
